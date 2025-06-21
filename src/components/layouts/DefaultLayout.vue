c3918725e0754c407048598fddb1614a87c1760b
Pavel Borisov <pavellbor@gmail.com>
2025-06-21 15:59:26 +0300

    Add auth nav in DefaultLayout

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -1,6 +1,39 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isAuthenticated = ref(false);
+</script>
+
 <template>
   <header>
-    <nav class="navbar navbar-light">
+    <nav v-if="isAuthenticated" class="navbar navbar-light">
+      <div class="container">
+        <RouterLink class="navbar-brand" to="/">conduit</RouterLink>
+        <ul class="nav navbar-nav pull-xs-right">
+          <li class="nav-item">
+            <!-- Add "active" class when you're on that page" -->
+            <RouterLink class="nav-link active" to="/">Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/editor">
+              <i class="ion-compose"></i>&nbsp;New Article
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/settings">
+              <i class="ion-gear-a"></i>&nbsp;Settings
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/profile/eric-simons">
+              <img src="" class="user-pic" />
+              Eric Simons
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <nav v-else class="navbar navbar-light">
       <div class="container">
         <RouterLink class="navbar-brand" to="/">conduit</RouterLink>
         <ul class="nav navbar-nav pull-xs-right">
